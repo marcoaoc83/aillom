5142ccb2277d4b024df09d5a37bf440c3f43e29d
marcoaoc83 <marcoaoc83@gmail.com>
2025-01-22 21:10:37 -0400

    Postgres

--- database/seeders/DumpCepDadosSeeder.php
+++ database/seeders/DumpCepDadosSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
-
+use Illuminate\Support\Facades\File;
 class DumpCepDadosSeeder extends Seeder
 {
     public function run(): void
@@ -19,40 +19,62 @@ class DumpCepDadosSeeder extends Seeder
         $driver = config('database.default'); // Driver em uso
 
         // Caminho do arquivo compactado e descompactado
-        $compressedPath = database_path('seeders/sql/dump-cep.sql.gz');
-        $decompressedPath = database_path('seeders/sql/dump-cep.sql');
+        $compressedPathCsv = database_path('seeders/csv/addresses.csv.gz');
+        $compressedPathSql = database_path('seeders/sql/dump-cep.sql.gz');
+        $decompressedPathCsv = database_path('seeders/csv/addresses.csv');
+        $decompressedPathSql = database_path('seeders/sql/dump-cep.sql');
 
-        // Descompactar o arquivo SQL, se necessário
-        if (!file_exists($decompressedPath)) {
-            $this->command->info('Descompactando o arquivo SQL...');
-            $process = Process::fromShellCommandline(sprintf('gunzip -c %s > %s', escapeshellarg($compressedPath), escapeshellarg($decompressedPath)));
+        // Garantir que os diretórios de destino existem
+        File::ensureDirectoryExists(database_path('seeders/csv'));
+        File::ensureDirectoryExists(database_path('seeders/sql'));
+
+        // Descompactar o arquivo CSV, se necessário
+        if (!file_exists($decompressedPathCsv)) {
+            $this->command->info('Descompactando o arquivo CSV...');
+            $processCsv = Process::fromShellCommandline(sprintf('gunzip -c %s > %s', escapeshellarg($compressedPathCsv), escapeshellarg($decompressedPathCsv)));
 
             try {
-                $process->mustRun();
-                $this->command->info('Arquivo descompactado com sucesso.');
+                $processCsv->mustRun();
+                $this->command->info('Arquivo CSV descompactado com sucesso.');
             } catch (ProcessFailedException $exception) {
-                $this->command->error('Falha ao descompactar o arquivo: ' . $exception->getMessage());
+                $this->command->error('Falha ao descompactar o arquivo CSV: ' . $exception->getMessage());
                 return;
             }
         }
 
-        // Importar o arquivo SQL para o banco de dados
-        $this->command->info('Iniciando a importação do arquivo SQL de CEP...');
+        // Descompactar o arquivo SQL, se necessário
+        if (!file_exists($decompressedPathSql)) {
+            $this->command->info('Descompactando o arquivo SQL...');
+            $processSql = Process::fromShellCommandline(sprintf('gunzip -c %s > %s', escapeshellarg($compressedPathSql), escapeshellarg($decompressedPathSql)));
+
+            try {
+                $processSql->mustRun();
+                $this->command->info('Arquivo SQL descompactado com sucesso.');
+            } catch (ProcessFailedException $exception) {
+                $this->command->error('Falha ao descompactar o arquivo SQL: ' . $exception->getMessage());
+                return;
+            }
+        }
+
+
+
+        // Importar o arquivo CSV para o banco de dados
+        $this->command->info('Iniciando a importação do arquivo de endereços...');
         switch ($driver) {
             case 'mysql':
-                $this->importForMysql($decompressedPath, $username, $password, $host, $port, $database);
+                $this->importForMysql($decompressedPathSql, $username, $password, $host, $port, $database);
                 break;
 
             case 'pgsql':
-                $this->importForPostgresql($decompressedPath, $username, $password, $host, $port, $database);
+                $this->importForPostgresql($compressedPathCsv, $username, $password, $host, $port, $database);
                 break;
 
             case 'sqlsrv':
-                $this->importForSqlServer($decompressedPath, $username, $password, $host, $port, $database);
+                $this->importForSqlServer($compressedPathCsv, $username, $password, $host, $port, $database);
                 break;
 
             case 'sqlite':
-                $this->importForSqlite($decompressedPath, $database);
+                $this->importForSqlite($compressedPathCsv, $database);
                 break;
 
             default:
@@ -78,56 +100,81 @@ class DumpCepDadosSeeder extends Seeder
         $this->runCommand($command, 'MySQL');
     }
 
+
+
     /**
-     * Importar para PostgreSQL.
+     * Importar CSV para PostgreSQL.
      */
     private function importForPostgresql(string $filePath, string $username, string $password, string $host, string $port, string $database): void
     {
         // Verificar se o arquivo existe
         if (!file_exists($filePath)) {
-            throw new \Exception("O arquivo SQL {$filePath} não foi encontrado.");
+            throw new \Exception("O arquivo CSV {$filePath} não foi encontrado.");
         }
 
-        // Definir o comando com PGPASSWORD
-        $command = sprintf(
-            'PGPASSWORD=%s psql -U %s -h %s -p %s -d %s -f %s',
+        // Comando para desabilitar os gatilhos
+        $disableTrigger = sprintf(
+            'PGPASSWORD=%s psql -U %s -h %s -p %s -d %s -c "ALTER TABLE addresses DISABLE TRIGGER ALL;"',
+            escapeshellarg($password),
+            escapeshellarg($username),
+            escapeshellarg($host),
+            escapeshellarg($port),
+            escapeshellarg($database)
+        );
+
+        // Comando para importar os dados
+        $copyCommand = sprintf(
+            'PGPASSWORD=%s psql -U %s -h %s -p %s -d %s -c "\COPY addresses(id, parent_id, description, abbreviation, postal_code, latitude, longitude, ibge_code, area_code, country_code, hierarchical_code, created_at, updated_at, deleted_at) FROM \'%s\' DELIMITER \',\' CSV HEADER;"',
             escapeshellarg($password),
             escapeshellarg($username),
             escapeshellarg($host),
             escapeshellarg($port),
             escapeshellarg($database),
-            escapeshellarg($filePath)
+            $filePath
         );
 
-        $this->runCommand($command, 'PostgreSQL');
+        // Comando para reativar os gatilhos
+        $enableTrigger = sprintf(
+            'PGPASSWORD=%s psql -U %s -h %s -p %s -d %s -c "ALTER TABLE addresses ENABLE TRIGGER ALL;"',
+            escapeshellarg($password),
+            escapeshellarg($username),
+            escapeshellarg($host),
+            escapeshellarg($port),
+            escapeshellarg($database)
+        );
+
+        // Executar os comandos com mensagens informativas
+        $this->runCommand($disableTrigger, 'PostgreSQL', 'Desativando gatilhos para a tabela addresses...');
+        $this->runCommand($copyCommand, 'PostgreSQL', 'Importando os dados do arquivo CSV para a tabela addresses...');
+        $this->runCommand($enableTrigger, 'PostgreSQL', 'Reativando gatilhos para a tabela addresses...');
     }
 
+
     /**
-     * Importar para SQL Server.
+     * Importar CSV para SQL Server.
      */
     private function importForSqlServer(string $filePath, string $username, string $password, string $host, string $port, string $database): void
     {
-        // SQLCMD é usado para importar no SQL Server
         $command = sprintf(
-            'sqlcmd -S %s,%s -U %s -P %s -d %s -i %s',
+            'bcp %s in %s -c -t, -F 2 -S %s,%s -U %s -P %s',
+            escapeshellarg($database . '.dbo.addresses'),
+            escapeshellarg($filePath),
             escapeshellarg($host),
             escapeshellarg($port),
             escapeshellarg($username),
-            escapeshellarg($password),
-            escapeshellarg($database),
-            escapeshellarg($filePath)
+            escapeshellarg($password)
         );
 
         $this->runCommand($command, 'SQL Server');
     }
 
     /**
-     * Importar para SQLite.
+     * Importar CSV para SQLite.
      */
     private function importForSqlite(string $filePath, string $database): void
     {
         $command = sprintf(
-            'sqlite3 %s < %s',
+            'sqlite3 %s ".mode csv" ".import %s addresses"',
             escapeshellarg($database),
             escapeshellarg($filePath)
         );
@@ -138,14 +185,17 @@ class DumpCepDadosSeeder extends Seeder
     /**
      * Executa o comando no terminal.
      */
-    private function runCommand(string $command, string $driver): void
+    private function runCommand(string $command, string $driver, string $info = null): void
     {
         $process = Process::fromShellCommandline($command);
         $process->setTimeout(0);
 
         try {
             $process->mustRun();
-            $this->command->info("Importação para {$driver} concluída com sucesso.");
+            if ($info)
+                $this->command->info($info);
+            else
+                $this->command->info("Importação para {$driver} concluída com sucesso.");
         } catch (ProcessFailedException $exception) {
             $this->command->error("Falha na importação para {$driver}: " . $exception->getMessage());
             $this->command->line($exception->getProcess()->getOutput());
